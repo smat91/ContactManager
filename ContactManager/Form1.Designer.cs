8d55ab21e8a3ea86b253393fbf38104b742c318c
Matthias Stieger <matthiass413@gmail.com>
2021-07-11 16:24:47 +0200

    Add test buttons

--- ContactManager/Form1.Designer.cs
+++ ContactManager/Form1.Designer.cs
@@ -30,6 +30,10 @@ namespace ContactManager
         private void InitializeComponent()
         {
             this.TxtOutput = new System.Windows.Forms.TextBox();
+            this.CmdReadAll = new System.Windows.Forms.Button();
+            this.CmdReadCustomers = new System.Windows.Forms.Button();
+            this.CmdReadEmployees = new System.Windows.Forms.Button();
+            this.CmdReadTrainees = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TxtOutput
@@ -37,14 +41,59 @@ namespace ContactManager
             this.TxtOutput.Location = new System.Drawing.Point(14, 92);
             this.TxtOutput.Multiline = true;
             this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.TxtOutput.Size = new System.Drawing.Size(522, 351);
             this.TxtOutput.TabIndex = 0;
+            // 
+            // CmdReadAll
+            // 
+            this.CmdReadAll.Location = new System.Drawing.Point(14, 13);
+            this.CmdReadAll.Name = "CmdReadAll";
+            this.CmdReadAll.Size = new System.Drawing.Size(75, 45);
+            this.CmdReadAll.TabIndex = 1;
+            this.CmdReadAll.Text = "read all";
+            this.CmdReadAll.UseVisualStyleBackColor = true;
+            this.CmdReadAll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CmdReadCustomers
+            // 
+            this.CmdReadCustomers.Location = new System.Drawing.Point(96, 12);
+            this.CmdReadCustomers.Name = "CmdReadCustomers";
+            this.CmdReadCustomers.Size = new System.Drawing.Size(75, 46);
+            this.CmdReadCustomers.TabIndex = 2;
+            this.CmdReadCustomers.Text = "read customers";
+            this.CmdReadCustomers.UseVisualStyleBackColor = true;
+            this.CmdReadCustomers.Click += new System.EventHandler(this.CmdReadCustomers_Click);
+            // 
+            // CmdReadEmployees
+            // 
+            this.CmdReadEmployees.Location = new System.Drawing.Point(178, 13);
+            this.CmdReadEmployees.Name = "CmdReadEmployees";
+            this.CmdReadEmployees.Size = new System.Drawing.Size(75, 45);
+            this.CmdReadEmployees.TabIndex = 3;
+            this.CmdReadEmployees.Text = "read emploees";
+            this.CmdReadEmployees.UseVisualStyleBackColor = true;
+            this.CmdReadEmployees.Click += new System.EventHandler(this.CmdReadEmployees_Click);
+            // 
+            // CmdReadTrainees
+            // 
+            this.CmdReadTrainees.Location = new System.Drawing.Point(260, 13);
+            this.CmdReadTrainees.Name = "CmdReadTrainees";
+            this.CmdReadTrainees.Size = new System.Drawing.Size(75, 45);
+            this.CmdReadTrainees.TabIndex = 4;
+            this.CmdReadTrainees.Text = "read trainees";
+            this.CmdReadTrainees.UseVisualStyleBackColor = true;
+            this.CmdReadTrainees.Click += new System.EventHandler(this.CmdReadTrainees_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.CmdReadTrainees);
+            this.Controls.Add(this.CmdReadEmployees);
+            this.Controls.Add(this.CmdReadCustomers);
+            this.Controls.Add(this.CmdReadAll);
             this.Controls.Add(this.TxtOutput);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +105,10 @@ namespace ContactManager
         #endregion
 
         private System.Windows.Forms.TextBox TxtOutput;
+        private System.Windows.Forms.Button CmdReadAll;
+        private System.Windows.Forms.Button CmdReadCustomers;
+        private System.Windows.Forms.Button CmdReadEmployees;
+        private System.Windows.Forms.Button CmdReadTrainees;
     }
 }
 
--- ContactManager/Form1.cs
+++ ContactManager/Form1.cs
@@ -15,11 +15,51 @@ namespace ContactManager
 {
     public partial class Form1 : Form
     {
+        public XDocument xdocument = XDocument.Load("..\\..\\..\\Persons.xml");
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TxtOutput.Clear();
+            IEnumerable<XElement> persons = xdocument.Elements();
+            foreach (var person in persons)
+            {
+                TxtOutput.Text = person + "\r\n";
+            }
+        }
+
+        private void CmdReadCustomers_Click(object sender, EventArgs e)
+        {
+            TxtOutput.Clear();
+            IEnumerable<XElement> customers = xdocument.Elements();
+            foreach (XElement customer in customers.Descendants ("Customer"))
+            {
+                TxtOutput.Text = customer + "\r\n";
+            }
+        }
+
+        private void CmdReadEmployees_Click(object sender, EventArgs e)
+        {
+            TxtOutput.Clear();
+            IEnumerable<XElement> employees = xdocument.Elements();
+            foreach (XElement employee in employees.Descendants("Employee"))
+            {
+                TxtOutput.Text = employee + "\r\n";
+            }
+        }
+
+        private void CmdReadTrainees_Click(object sender, EventArgs e)
+        {
+            TxtOutput.Clear();
+            IEnumerable<XElement> trainees = xdocument.Elements();
+            foreach (XElement trainee in trainees.Descendants("Trainee"))
+            {
+                TxtOutput.Text = trainee + "\r\n";
+            }
+        }
     }
 }
