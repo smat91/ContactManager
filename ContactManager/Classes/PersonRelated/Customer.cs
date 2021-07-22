a890103ed3e687faf5647d519e8540e8e5a8ed1c
Matthias Stieger <matthiass413@gmail.com>
2021-07-22 22:09:33 +0200

    Add cutomer class

---
+++ ContactManager/Classes/PersonRelated/Customer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ContactManager.Classes.PersonRelated
+{
+    class Customer : Person
+    {
+        public string companyName_
+        {
+            get
+            {
+                return (string)
+                  (from element in person_.Descendants("CompanyName")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("CompanyName")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public string customerType_
+        {
+            get
+            {
+                return  (string)
+                  (from element in person_.Descendants("CustomerType")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value.Length != 1)
+                {
+                    const int a = 'A';
+                    const int e = 'E';
+
+                    int customerType = char.Parse(value);
+
+                    if (customerType >= a && customerType <= e)
+                    {
+                        (from element in person_.Descendants("CustomerType")
+                         select element).First().SetValue(value);
+                    }
+                    else 
+                    { 
+                        throw new ArgumentException("value must be a capital letter and between A and E!");
+                    }
+                }
+                else 
+                {
+                    throw new ArgumentException("value must contain one character!");
+                }
+                
+            }
+        }
+
+        public Customer(ref IEnumerable<XElement> person)
+            : base(ref person)
+        {
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", " + companyName_ + ", " + customerType_;
+        }
+    }
+}
