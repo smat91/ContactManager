382e620672dbaf4a4414585821de6f01d08a67a1
Milad Fakiry <m.fakiry3@gmail.com>
2021-07-25 14:40:33 +0200

    Get und Set für Klasse Person eingestellt (zur Überlegung ist noch Adresse und Kontaktdaten UND set MEthode für INT Variablen)

--- ContactManager/Classes/PersonRelated/Person.cs
+++ ContactManager/Classes/PersonRelated/Person.cs
@@ -10,6 +10,76 @@ namespace ContactManager
 {
     public class Person
     {
+        public int id_
+        {
+            get
+            {
+                return (int)
+                    (from element in person_.Descendants("Id")
+                     select element).First();
+            }
+
+            set
+            {
+                if (value != null) //Int immer true, was brauchen wir hier?
+                {
+                    (from element in person_.Descendants("Id")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public string state_
+        {
+            get
+            {
+                return (string)
+                    (from element in person_.Descendants("State")
+                     select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("State")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public string salutation_
+        {
+            get
+            {
+                return (string)
+                    (from element in person_.Descendants("Salutation")
+                     select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Salutation")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+
+        
+        }
         public string firstname_ 
         {
             get 
@@ -47,6 +117,191 @@ namespace ContactManager
                 if (value != null)
                 {
                     (from element in person_.Descendants("Lastname")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public string sex_
+        {
+            get
+            {
+                return (string)
+                  (from element in person_.Descendants("Sex")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Sex")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public string function_
+        {
+            get
+            {
+                return (string)
+                  (from element in person_.Descendants("Function")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Function")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public string street_
+        {
+            get
+            {
+                return (string)
+                  (from element in person_.Descendants("Street")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Street")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public int number_
+        {
+            get
+            {
+                return (int)
+                  (from element in person_.Descendants("Number")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Number")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public string city_
+        {
+            get
+            {
+                return (string)
+                  (from element in person_.Descendants("City")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("City")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public int zip_
+        {
+            get
+            {
+                return (int)
+                  (from element in person_.Descendants("Zip")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Zip")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+
+        public string phoneBusiness_
+        {
+            get
+            {
+                return (string)
+                  (from element in person_.Descendants("Phone<business>")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Phone<business>")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public string phoneMob_
+        {
+            get
+            {
+                return (string)
+                  (from element in person_.Descendants("Phone<mobile>")
+                   select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Phone<mobile>")
                      select element).First().SetValue(value);
                 }
                 else
