1358b3e4c127595ce3a02e7e25d3c433c15234c0
Milad Fakiry <m.fakiry3@gmail.com>
2021-08-08 23:10:07 +0200

    Employee and some comments added

--- ContactManager/Classes/PersonRelated/Employee.cs
+++ ContactManager/Classes/PersonRelated/Employee.cs
@@ -47,16 +47,159 @@ namespace ContactManager.Classes.PersonRelated
                 (from element in person_.Descendants("DateOfBirth")
                  select element).First().SetValue(value);
             }
+        }public string insuranceNumber_
+        {
+            get
+            {
+                return (string)
+                    (from element in person_.Descendants("InsuranceNumber"
+                     select element).First();
+            }
+
+            set
+            { 
+                if (value != null)
+                {
+                    (from element in person_.Descendants("InsuranceNumber")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
         }
+
+        public string citizenship_
+        {
+            get
+            {
+                retung(string)
+                    (from element in person_.Descendants("Citizenship")
+                     select element).First();
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    (from element in person_.Descendants("Citizenship")
+                     select element).First().SetValue(value);
+                }
+                else
+                {
+                    throw new ArgumentException("value cannot be null!");
+                }
+            }
+        }
+
+        public DateTime entryDate_
+        {
+            get
+            {
+                
+                return (DateTime)
+                  (from element in person_.Descendants("EntryDate")
+                   select element).First();
+            }
+
+            set
+            {
+                (from element in person_.Descendants("EntryDate")
+                 select element).First().SetValue(value);
+            }
+        }
+
+        public DateTime seperationDate_
+        {
+            get
+            {
+                
+                return (DateTime)
+                  (from element in person_.Descendants("SeperationDate")
+                   select element).First();
+            }
+
+            set
+            {
+                (from element in person_.Descendants("SeperationDate")
+                 select element).First().SetValue(value);
+            }
+        }
+
+        public int levelOfEmployment_
+        {
+             get
+            {
+                // return gibt einen Int zurück / Descendants liefert eine Sammlung der Nachfahrenelemente für
+                // dieses Dokument oder Element in der Reihenfolge der Dokumente.
+                return (int)
+                    (from element in person_.Descendants("LevelOfEmployment")
+                     select element).First();
+            }
+
+            set
+            {
+                
+                    (from element in person_.Descendants("LevelOfEmployment")
+                     select element).First().SetValue(value);
+            }
+        }
+
+        public int level_
+        {
+             get
+            {
+                // return gibt einen Int zurück / Descendants liefert eine Sammlung der Nachfahrenelemente für
+                // dieses Dokument oder Element in der Reihenfolge der Dokumente.
+                return (int)
+                    (from element in person_.Descendants("Level")
+                     select element).First();
+            }
+
+            set
+            {
+                
+                    (from element in person_.Descendants("Level")
+                     select element).First().SetValue(value);
+            }
+        }
+
+
 
         public Employee(ref IEnumerable<XElement> person)
             : base(ref person)
         {
+            employee_ = Employee;
         }
 
         public override string ToString()
         {
-            return base.ToString() + ", " + department_ + ", " + birthday_.ToShortDateString();
+            return base.ToString() + ", " + department_ + ", " + birthday_.ToShortDateString() + ", " + insuranceNumber_ ", " + citizenship_ + ", " + citizenship_ + ", " + entryDate_ + ", " + seperetionDate_ + ", " + levelOfEmployment_ + ", " + level_;
+        }
+
+        
+        // Mit diesem Befehl wird ein XElement Template von Person erzeugt und ausgegenben
+        public XElement XelementTemplate()
+        {
+            return new XElement("Employee",
+                            new XElement("DateOfBirth", ""),
+                            new XElement("Department", ""),
+                            new XElement("InsuranceNumber", ""),
+                            new XElement("Citizenship", ""),
+                            new XElement("EntryDate", ""),
+                            new XElement("SeparationDate", ""),
+                            new XElement("LevelOfEmployment", ""),
+                            new XElement("Level", "");
+        }
+
+         // Hier wird der Vergleich gemacht, ob die Instanzen gleich sind wie im XML, wenn nicht, dann kommt "false"
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj) &&
+                companyName_ == obj.companyName_ &&
+                customerType_ == obj.customerType_ &&
+                fax_ == obj.fax_;
         }
     }
 }
