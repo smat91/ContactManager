8653dd242274079f04ff5fbc577b56c28e672581
billeberhard <bill.eberhard193e@gmail.com>
2021-08-10 15:25:12 +0200

    UC Top / Side Bar erstellt UCHome erstellt

--- ContactManager/UI/Form1.Designer.cs
+++ ContactManager/UI/Form1.Designer.cs
@@ -29,25 +29,29 @@ namespace ContactManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.TxtOutput = new System.Windows.Forms.TextBox();
             this.CmdReadAll = new System.Windows.Forms.Button();
             this.CmdReadCustomers = new System.Windows.Forms.Button();
             this.CmdReadEmployees = new System.Windows.Forms.Button();
             this.CmdReadTrainees = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.TxtOutput = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // TxtOutput
-            // 
-            this.TxtOutput.Location = new System.Drawing.Point(14, 92);
-            this.TxtOutput.Multiline = true;
-            this.TxtOutput.Name = "TxtOutput";
-            this.TxtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.TxtOutput.Size = new System.Drawing.Size(522, 351);
-            this.TxtOutput.TabIndex = 0;
             // 
             // CmdReadAll
             // 
-            this.CmdReadAll.Location = new System.Drawing.Point(14, 13);
+            this.CmdReadAll.Location = new System.Drawing.Point(183, 12);
             this.CmdReadAll.Name = "CmdReadAll";
             this.CmdReadAll.Size = new System.Drawing.Size(75, 45);
             this.CmdReadAll.TabIndex = 1;
@@ -57,7 +61,7 @@ namespace ContactManager
             // 
             // CmdReadCustomers
             // 
-            this.CmdReadCustomers.Location = new System.Drawing.Point(96, 12);
+            this.CmdReadCustomers.Location = new System.Drawing.Point(264, 11);
             this.CmdReadCustomers.Name = "CmdReadCustomers";
             this.CmdReadCustomers.Size = new System.Drawing.Size(75, 46);
             this.CmdReadCustomers.TabIndex = 2;
@@ -66,7 +70,7 @@ namespace ContactManager
             // 
             // CmdReadEmployees
             // 
-            this.CmdReadEmployees.Location = new System.Drawing.Point(178, 13);
+            this.CmdReadEmployees.Location = new System.Drawing.Point(345, 11);
             this.CmdReadEmployees.Name = "CmdReadEmployees";
             this.CmdReadEmployees.Size = new System.Drawing.Size(75, 45);
             this.CmdReadEmployees.TabIndex = 3;
@@ -75,37 +79,186 @@ namespace ContactManager
             // 
             // CmdReadTrainees
             // 
-            this.CmdReadTrainees.Location = new System.Drawing.Point(260, 13);
+            this.CmdReadTrainees.Location = new System.Drawing.Point(426, 12);
             this.CmdReadTrainees.Name = "CmdReadTrainees";
             this.CmdReadTrainees.Size = new System.Drawing.Size(75, 45);
             this.CmdReadTrainees.TabIndex = 4;
             this.CmdReadTrainees.Text = "read trainees";
             this.CmdReadTrainees.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 595);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.CmdReadAll);
+            this.panel2.Controls.Add(this.CmdReadCustomers);
+            this.panel2.Controls.Add(this.CmdReadTrainees);
+            this.panel2.Controls.Add(this.CmdReadEmployees);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(164, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(908, 77);
+            this.panel2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(0, 113);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(0, 148);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(0, 183);
+            this.button4.Margin = new System.Windows.Forms.Padding(0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(164, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(0, 218);
+            this.button5.Margin = new System.Windows.Forms.Padding(0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(164, 35);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(0, 253);
+            this.button6.Margin = new System.Windows.Forms.Padding(0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(164, 35);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(0, 288);
+            this.button7.Margin = new System.Windows.Forms.Padding(0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(164, 35);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(0, 323);
+            this.button8.Margin = new System.Windows.Forms.Padding(0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(164, 35);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(0, 358);
+            this.button9.Margin = new System.Windows.Forms.Padding(0);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(164, 35);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // TxtOutput
+            // 
+            this.TxtOutput.Location = new System.Drawing.Point(190, 120);
+            this.TxtOutput.Multiline = true;
+            this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtOutput.Size = new System.Drawing.Size(870, 425);
+            this.TxtOutput.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(546, 450);
-            this.Controls.Add(this.CmdReadTrainees);
-            this.Controls.Add(this.CmdReadEmployees);
-            this.Controls.Add(this.CmdReadCustomers);
-            this.Controls.Add(this.CmdReadAll);
+            this.ClientSize = new System.Drawing.Size(1072, 595);
             this.Controls.Add(this.TxtOutput);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox TxtOutput;
         private System.Windows.Forms.Button CmdReadAll;
         private System.Windows.Forms.Button CmdReadCustomers;
         private System.Windows.Forms.Button CmdReadEmployees;
         private System.Windows.Forms.Button CmdReadTrainees;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox TxtOutput;
     }
 }
 
---
+++ ContactManager/UI/UCHome.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace ContactManager.UI
+{
+    partial class UCHome
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TxtOutput
+            // 
+            this.TxtOutput.Location = new System.Drawing.Point(182, 160);
+            this.TxtOutput.Multiline = true;
+            this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtOutput.Size = new System.Drawing.Size(870, 425);
+            this.TxtOutput.TabIndex = 1;
+            // 
+            // UCHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TxtOutput);
+            this.Name = "UCHome";
+            this.Size = new System.Drawing.Size(1088, 634);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtOutput;
+    }
+}
