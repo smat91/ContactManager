3c7aba0d2bc0163b0d0096897a2b4bce7ca8302c
Matthias Stieger <matthiass413@gmail.com>
2021-08-10 11:25:52 +0200

    Add missing methods

--- ContactManager/Classes/PersonRelated/Trainee.cs
+++ ContactManager/Classes/PersonRelated/Trainee.cs
@@ -21,14 +21,14 @@ namespace ContactManager.Classes.PersonRelated
 
             set
             {
-                if (value != 0)
+                if (value > 0)
                 {
                     (from element in person_.Descendants("NumberOfTraineeYears")
                      select element).First().SetValue(value);
                 }
                 else
                 {
-                    throw new ArgumentException("value cannot be null!");
+                    throw new ArgumentException("value must be greater than 0");
                 }
             }
         }
@@ -44,27 +44,57 @@ namespace ContactManager.Classes.PersonRelated
 
             set
             {
-                if (value != 0)
+                if (value > 0 && value <= numberOfTraineeYears_)
                 {
                     (from element in person_.Descendants("CurrentTraineeYear")
                      select element).First().SetValue(value);
                 }
                 else
                 {
-                    throw new ArgumentException("value cannot be null!");
+                    throw new ArgumentException("value must be greater than 0 and smaller or equal to numberOfTraineeYears_");
                 }
             }
         }
 
+        // Mit diesem Befehl wird eine Sammlung von Elementen in der Dokumentenreihenfolge zurückgegeben
+        public IEnumerable<XElement> trainee_;
 
-        public Trainee(ref IEnumerable<XElement> person)
-            : base(ref person)
+        public Trainee(ref IEnumerable<XElement> trainee)
+            : base(ref trainee)
         {
+            trainee_ = trainee;
+        }
+
+        // Mit dieser Methode wird ein XElement Template von Employee erzeugt und ausgegenben
+        public override XElement XelementTemplate()
+        {
+            XElement trainee = base.XelementTemplate();
+
+            // Person Element von Basis nach Customer umbenennen
+            trainee.Element("Person").Name = "Trainee";
+
+            // Neue mitarbeiterbezogene Allgemeinattribute hinzufügen
+            trainee.Descendants("Function").FirstOrDefault()
+                .AddAfterSelf(new XElement("NumberOfTraineeYears", ""),
+                              new XElement("CurrentTraineeYear", ""));
+
+            return trainee;
+        }
+
+        // Hier wird der Vergleich gemacht, ob die Instanzen gleich sind wie im XML, wenn nicht, dann kommt "false"
+        public override bool Equals(object obj)
+        {
+            return obj is Trainee trainee &&
+                   base.Equals(trainee) &&
+                   numberOfTraineeYears_ == trainee.numberOfTraineeYears_ &&
+                   currentTraineeYear_ == trainee.currentTraineeYear_;
         }
 
         public override string ToString()
         {
-            return base.ToString() + ", " + numberOfTraineeYears_ + ", " + currentTraineeYear_;
+            return base.ToString() + ", " + 
+                numberOfTraineeYears_ + ", " + 
+                currentTraineeYear_;
         }
     }
 }
