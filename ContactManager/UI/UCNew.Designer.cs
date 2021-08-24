231ae4bff4ebe9c22cec5b3f0822d5ea9171c69d
Nico Varela Garcia <nico.varelagarcia@gmail.com>
2021-08-24 15:27:14 +0200

    UC Files erstellt

---
+++ ContactManager/UI/UCNew.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace ContactManager.UI
+{
+    partial class UCNew
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblTitle = new System.Windows.Forms.Label();
+            this.TxtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LblTitle
+            // 
+            this.LblTitle.AutoSize = true;
+            this.LblTitle.Location = new System.Drawing.Point(16, 14);
+            this.LblTitle.Name = "LblTitle";
+            this.LblTitle.Size = new System.Drawing.Size(76, 15);
+            this.LblTitle.TabIndex = 0;
+            this.LblTitle.Text = "Neuer Kunde";
+            // 
+            // TxtOutput
+            // 
+            this.TxtOutput.Location = new System.Drawing.Point(16, 55);
+            this.TxtOutput.Multiline = true;
+            this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.Size = new System.Drawing.Size(785, 324);
+            this.TxtOutput.TabIndex = 1;
+            // 
+            // UCNew
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TxtOutput);
+            this.Controls.Add(this.LblTitle);
+            this.Name = "UCNew";
+            this.Size = new System.Drawing.Size(870, 425);
+            this.Load += new System.EventHandler(this.UCNew_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblTitle;
+        private System.Windows.Forms.TextBox TxtOutput;
+    }
+}
---
+++ ContactManager/UI/UCNew.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ContactManager.UI
+{
+    public partial class UCNew : UserControl
+    {
+        public UCNew()
+        {
+            InitializeComponent();
+        }
+
+        private XmlTemplate xmlTemplate = new XmlTemplate();
+
+        private void UCNew_Load(object sender, EventArgs e)
+        {
+            XElement customerTemplate = xmlTemplate.XelementTemplateCustomer();
+            Customer c1 = new Customer(ref customerTemplate);
+
+            c1.firstname_ = 1;
+
+            TxtOutput.Text += c1.id_;
+
+            foreach (var person in customerTemplate.Descendants())
+            {
+                TxtOutput.Text += person + "\r\n";
+            }
+        }
+    }
+}
