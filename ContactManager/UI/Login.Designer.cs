bea6f6ac89545748f1e45ab0f9bd40fc13b0deaa
Matthias Stieger <matthiass413@gmail.com>
2021-09-05 11:38:49 +0200

    Add Login to application

---
+++ ContactManager/UI/Login.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace ContactManager
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblUser = new System.Windows.Forms.Label();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.TxtUser = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.CmdLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblUser
+            // 
+            this.LblUser.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.LblUser.AutoSize = true;
+            this.LblUser.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.LblUser.Location = new System.Drawing.Point(12, 23);
+            this.LblUser.Name = "LblUser";
+            this.LblUser.Size = new System.Drawing.Size(86, 15);
+            this.LblUser.TabIndex = 0;
+            this.LblUser.Text = "Benutzername:";
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.LblPassword.Location = new System.Drawing.Point(12, 74);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(57, 15);
+            this.LblPassword.TabIndex = 1;
+            this.LblPassword.Text = "Passwort:";
+            // 
+            // TxtUser
+            // 
+            this.TxtUser.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.TxtUser.Location = new System.Drawing.Point(107, 20);
+            this.TxtUser.Name = "TxtUser";
+            this.TxtUser.Size = new System.Drawing.Size(249, 23);
+            this.TxtUser.TabIndex = 2;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.TxtPassword.Location = new System.Drawing.Point(107, 71);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.Size = new System.Drawing.Size(249, 23);
+            this.TxtPassword.TabIndex = 3;
+            this.TxtPassword.UseSystemPasswordChar = true;
+            // 
+            // CmdLogin
+            // 
+            this.CmdLogin.Location = new System.Drawing.Point(12, 136);
+            this.CmdLogin.Name = "CmdLogin";
+            this.CmdLogin.Size = new System.Drawing.Size(343, 40);
+            this.CmdLogin.TabIndex = 4;
+            this.CmdLogin.Text = "Login";
+            this.CmdLogin.UseVisualStyleBackColor = true;
+            this.CmdLogin.Click += new System.EventHandler(this.CmdLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.ClientSize = new System.Drawing.Size(368, 191);
+            this.Controls.Add(this.CmdLogin);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUser);
+            this.Controls.Add(this.LblPassword);
+            this.Controls.Add(this.LblUser);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblUser;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.TextBox TxtUser;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button CmdLogin;
+    }
+}
---
+++ ContactManager/UI/Login.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactManager
+{
+    public partial class Login : Form
+    {
+        int loginTrys = 0;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdLogin_Click(object sender, EventArgs e)
+        {
+            // User = Admin !!! NUR ZU DEBUG ZWECKEN HIER KOMMENTAR VOR ROLLOUT LÖSCHEN !!!
+            string user = "c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f";
+
+            // PW = Admin2021Q3 !!! NUR ZU DEBUG ZWECKEN HIER KOMMENTAR VOR ROLLOUT LÖSCHEN !!!
+            string password = "4b84cf5edf20d6dbf0d25b37f4d4eae2f7faaf2fdf4fe4e6b4dd9f9b3df2be0c";
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                if (VerifyHash(sha256Hash, TxtUser.Text, user) && VerifyHash(sha256Hash, TxtPassword.Text, password))
+                {
+                    this.Hide();
+                    Form1 f1 = new Form1();
+                    f1.ShowDialog();
+                }
+                else
+                {
+                    ++loginTrys;
+
+                    string message = $"Password or username is invalid. Try {loginTrys} of 3.";
+                    string caption = "Error detected in input";
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+
+                    DialogResult  result = MessageBox.Show(message, caption, buttons);
+
+                    if (result == System.Windows.Forms.DialogResult.OK && loginTrys >= 3) {
+                        Application.Exit();
+                    }
+                }
+            }
+        }
+
+        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        private static bool VerifyHash(HashAlgorithm hashAlgorithm, string input, string hash)
+        {
+            // Hash the input.
+            var hashOfInput = GetHash(hashAlgorithm, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashOfInput, hash) == 0;
+        }
+    }
+}
