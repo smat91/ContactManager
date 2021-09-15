b1b8adc9bf0b18e7be31eb4c93f8e30bc2fbaa79
Milad Fakiry <m.fakiry3@gmail.com>
2021-09-15 21:16:57 +0200

    BackgroudImage bei buttons entfernt(Doppelt)

--- ContactManager/UI/UCTopBarResults.Designer.cs
+++ ContactManager/UI/UCTopBarResults.Designer.cs
@@ -29,7 +29,6 @@ namespace ContactManager.UI
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCTopBarResults));
             this.CmbFilter = new System.Windows.Forms.ComboBox();
             this.CmdSearch = new System.Windows.Forms.Button();
             this.TxtSearch = new System.Windows.Forms.TextBox();
@@ -42,9 +41,10 @@ namespace ContactManager.UI
             // CmbFilter
             // 
             this.CmbFilter.FormattingEnabled = true;
-            this.CmbFilter.Location = new System.Drawing.Point(460, 27);
+            this.CmbFilter.Location = new System.Drawing.Point(537, 31);
+            this.CmbFilter.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.CmbFilter.Name = "CmbFilter";
-            this.CmbFilter.Size = new System.Drawing.Size(125, 21);
+            this.CmbFilter.Size = new System.Drawing.Size(145, 23);
             this.CmbFilter.TabIndex = 15;
             this.CmbFilter.Click += new System.EventHandler(this.CmbFilter_Click);
             // 
@@ -54,11 +54,12 @@ namespace ContactManager.UI
             this.CmdSearch.Dock = System.Windows.Forms.DockStyle.Right;
             this.CmdSearch.FlatAppearance.BorderSize = 0;
             this.CmdSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CmdSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.CmdSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.CmdSearch.Image = global::ContactManager.Properties.Resources.search_2903;
-            this.CmdSearch.Location = new System.Drawing.Point(604, 0);
+            this.CmdSearch.Location = new System.Drawing.Point(706, 0);
+            this.CmdSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.CmdSearch.Name = "CmdSearch";
-            this.CmdSearch.Size = new System.Drawing.Size(81, 67);
+            this.CmdSearch.Size = new System.Drawing.Size(94, 77);
             this.CmdSearch.TabIndex = 14;
             this.CmdSearch.Text = "Suchen";
             this.CmdSearch.UseVisualStyleBackColor = true;
@@ -66,9 +67,10 @@ namespace ContactManager.UI
             // 
             // TxtSearch
             // 
-            this.TxtSearch.Location = new System.Drawing.Point(220, 27);
+            this.TxtSearch.Location = new System.Drawing.Point(257, 31);
+            this.TxtSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.TxtSearch.Name = "TxtSearch";
-            this.TxtSearch.Size = new System.Drawing.Size(197, 20);
+            this.TxtSearch.Size = new System.Drawing.Size(229, 23);
             this.TxtSearch.TabIndex = 13;
             // 
             // CmdNew
@@ -76,12 +78,13 @@ namespace ContactManager.UI
             this.CmdNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.CmdNew.FlatAppearance.BorderSize = 0;
             this.CmdNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CmdNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.CmdNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.CmdNew.Image = global::ContactManager.Properties.Resources.plus_945;
             this.CmdNew.Location = new System.Drawing.Point(0, 0);
+            this.CmdNew.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.CmdNew.Name = "CmdNew";
             this.CmdNew.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.CmdNew.Size = new System.Drawing.Size(64, 67);
+            this.CmdNew.Size = new System.Drawing.Size(75, 77);
             this.CmdNew.TabIndex = 12;
             this.CmdNew.Text = "Neu";
             this.CmdNew.UseVisualStyleBackColor = true;
@@ -89,16 +92,16 @@ namespace ContactManager.UI
             // 
             // CmdEdit
             // 
-            this.CmdEdit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("CmdEdit.BackgroundImage")));
             this.CmdEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.CmdEdit.FlatAppearance.BorderSize = 0;
             this.CmdEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CmdEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.CmdEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.CmdEdit.Image = global::ContactManager.Properties.Resources.pencil_938__2_;
-            this.CmdEdit.Location = new System.Drawing.Point(64, 0);
+            this.CmdEdit.Location = new System.Drawing.Point(75, 0);
+            this.CmdEdit.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.CmdEdit.Name = "CmdEdit";
             this.CmdEdit.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.CmdEdit.Size = new System.Drawing.Size(64, 67);
+            this.CmdEdit.Size = new System.Drawing.Size(75, 77);
             this.CmdEdit.TabIndex = 9;
             this.CmdEdit.Text = "Bearbeiten";
             this.CmdEdit.UseVisualStyleBackColor = true;
@@ -109,11 +112,12 @@ namespace ContactManager.UI
             this.CmdReset.Dock = System.Windows.Forms.DockStyle.Right;
             this.CmdReset.FlatAppearance.BorderSize = 0;
             this.CmdReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CmdReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.CmdReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.CmdReset.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.CmdReset.Location = new System.Drawing.Point(685, 0);
+            this.CmdReset.Location = new System.Drawing.Point(800, 0);
+            this.CmdReset.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.CmdReset.Name = "CmdReset";
-            this.CmdReset.Size = new System.Drawing.Size(93, 67);
+            this.CmdReset.Size = new System.Drawing.Size(108, 77);
             this.CmdReset.TabIndex = 11;
             this.CmdReset.Text = "Suche zurücksetzen";
             this.CmdReset.UseVisualStyleBackColor = true;
@@ -121,16 +125,16 @@ namespace ContactManager.UI
             // 
             // CmdDelete
             // 
-            this.CmdDelete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("CmdDelete.BackgroundImage")));
             this.CmdDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.CmdDelete.FlatAppearance.BorderSize = 0;
             this.CmdDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CmdDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.CmdDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.CmdDelete.Image = global::ContactManager.Properties.Resources.close_877;
-            this.CmdDelete.Location = new System.Drawing.Point(129, 0);
+            this.CmdDelete.Location = new System.Drawing.Point(150, 0);
+            this.CmdDelete.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.CmdDelete.Name = "CmdDelete";
             this.CmdDelete.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.CmdDelete.Size = new System.Drawing.Size(64, 67);
+            this.CmdDelete.Size = new System.Drawing.Size(75, 77);
             this.CmdDelete.TabIndex = 10;
             this.CmdDelete.Text = "Löschen";
             this.CmdDelete.UseVisualStyleBackColor = true;
@@ -138,7 +142,7 @@ namespace ContactManager.UI
             // 
             // UCTopBarResults
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.CmbFilter);
             this.Controls.Add(this.CmdSearch);
@@ -147,8 +151,9 @@ namespace ContactManager.UI
             this.Controls.Add(this.CmdEdit);
             this.Controls.Add(this.CmdReset);
             this.Controls.Add(this.CmdDelete);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "UCTopBarResults";
-            this.Size = new System.Drawing.Size(778, 67);
+            this.Size = new System.Drawing.Size(908, 77);
             this.ResumeLayout(false);
             this.PerformLayout();
 
